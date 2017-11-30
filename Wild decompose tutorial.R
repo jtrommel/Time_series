52fcf5b3febdabae5d769c6c8c070a9f3b12f90c
Jan Trommelmans <jan.trommelmans@me.com>
2017-11-30 17:10:32 +0100

    Multiplicatief model

--- Wild decompose tutorial.R
+++ Wild decompose tutorial.R
@@ -158,12 +158,76 @@ ggplot(data=ap.df.q.average, aes(x=quarter, y=avg, group=1)) +
 ap.df$ase <- rep(ap.df.q.average$avg,12)
 ap.df$pol.ase <- ap.df$pol +ap.df$ase
 ap.df$remainder <- ap.df$x - ap.df$pol.ase
-result <- data.frame(tijd=rep(ap.df$tijd,4),
-                     deco=c(ap.df$x,ap.df$pol,ap.df$ase,ap.df$remainder),
-                     type=factor(rep(c("original","trend","season","remainder"),each=nrow(ap.df)),
-                                 levels=c("original","trend","season","remainder")))
+result <- data.frame(tijd=rep(ap.df$tijd,5),
+                     deco=c(ap.df$x,ap.df$pol.ase,ap.df$pol,ap.df$ase,ap.df$remainder),
+                     type=factor(rep(c("original","pol+ase", "trend","season","remainder"),each=nrow(ap.df)),
+                                 levels=c("original","pol+ase", "trend","season","remainder")))
 ggplot(data=result, aes(x=tijd,y=deco)) +
   geom_line() +
   facet_grid(type ~ ., scales = "free_y", switch = "y") +
   labs(x="Date", y=NULL, title="Decompositie" , subtitle="Trend=poly, Season=ase")
 # Daar zit nog veel in de remainder!
+#
+# Wellicht is een multiplicatief model beter geschikt dan een additief omdat het
+# seizoenseffect stijgt samen met de trend. Een logaritme-transformatie maakt er
+# opnieuw een additief probleem van alhoewel ik niet goed begrijp dat de
+# error-term ook multiplicatief kan zijn.
+#
+# Goede uitleg: https://www.youtube.com/watch?v=CfB9ROwF2ew
+#
+ap.df$log <- log10(ap.df$x)
+ggplot(data=ap.df, aes(x=tijd,y=log)) + geom_line()
+#
+# De groei van het seizoensgebonden deel lijkt inderdaad minder te groeien
+# naarmate de trend groter wordt.
+#
+# Trend met een polynoom
+model.log.pol <- lm(ap.df$log ~ poly(ap.df$tijd,3))
+ap.df$log.trend <- model.log.pol$fitted.values
+ggplot(data=ap.df, aes(x=tijd)) +
+  geom_line(aes(y=log)) +
+  geom_line(aes(y=log.trend),color="red")
+#
+# Seizoensgebonden deel
+#
+# Groepering per quarter
+#
+ap.df %>% group_by(jaar,quarter) %>% summarise(som=sum(log)) -> ap.df.q.log
+ggplot(data=ap.df.q.log) + geom_point(aes(x=jaar,y=som,color=quarter))
+ggplot(data=ap.df.q.log,aes(x=quarter,y=som,group=jaar)) + geom_line(aes(color=as.factor(jaar)))
+#
+# mse = multiplicative seasonal effect
+#
+ap.df.q.log$mse <- 0
+for (i in c(1949:1960)) {
+  mod.log <- lm(ap.df.q.log$som[ap.df.q.log$jaar==i] ~ c(1:4))
+  ap.df.q.log$mse[ap.df.q.log$jaar==i] <- mod.log$residuals
+}
+ggplot(data=ap.df.q.log, aes(x=quarter, group=jaar, color=as.factor(jaar))) +
+  geom_line(aes(y=mse)) +
+  labs(title="multiplicative seasonal effect per year")
+#
+# Average seasonal effect
+#
+ap.df.q.log %>% group_by(quarter) %>% summarise(avg=mean(mse)) -> ap.df.q.log.average                                     
+ggplot(data=ap.df.q.log.average, aes(x=quarter, y=avg, group=1)) +
+  geom_line() +
+  labs(title="Multiplicative seasonal effect (average)")
+#
+# Adding the trend and the multiplicative seasonal effect and finding the remainder
+# For the trend I take the polynomial because at the moment it seems the simplest.
+# We gaan terug van getransformeerde waarden (log10) naar invers getransformeerde waarden (10^)
+#
+ap.df$mse <- rep(10^ap.df.q.log.average$avg,12)
+ap.df$pol.mse <- (10^ap.df$log.trend)*ap.df$mse
+ap.df$mse.remainder <- ap.df$x - ap.df$pol.mse
+result <- data.frame(tijd=rep(ap.df$tijd,5),
+                     deco=c(ap.df$x,ap.df$pol.mse,10^ap.df$log.trend,ap.df$mse,ap.df$mse.remainder),
+                     type=factor(rep(c("original","pol+mse", "trend","season","remainder"),each=nrow(ap.df)),
+                                 levels=c("original","pol+mse", "trend","season","remainder")))
+ggplot(data=result, aes(x=tijd,y=deco)) +
+  geom_line() +
+  facet_grid(type ~ ., scales = "free_y", switch = "y") +
+  labs(x="Date", y=NULL, title="Decompositie" , subtitle="Trend=poly, Season=mse")
+#
+# Beter, maar niet goed genoeg.
